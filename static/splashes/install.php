5b95b0f28a6b9cac1d317b15dc9cf0efa344093f
Anton Korotkov <antonkorotkoff@gmail.com>
2014-09-23 17:48:51 +0300

    splash sample

---
+++ static/splashes/install.php
@@ -0,0 +1,53 @@
+<div class="changelog">
+  
+  <div class="overview">
+
+    <h2>WP-Invoice 4.0.0 has beed installed</h2>
+
+    <p><?php _e( 'Congratulations! You have just installed brand new version of WP-Invoice plugin. There are some important things that you need to be aware of in order to use our products successfully and with pleasure.' ) ?></p>
+
+    <p><?php _e( 'Please read the following instructions carefully. Explore links below to get more information on our site.' ); ?></p>
+
+  </div>
+  
+  <hr />
+  
+  <div class="feature-section col two-col">
+    
+    <div class="col-1">
+      
+      <h3><?php _e( 'New license management system' ); ?></h3>
+      
+      <p>Information about licenses</p>
+      
+    </div>
+    
+    <div class="col-2 last-feature">
+      
+      <p>Some video or image goes here</p>
+      
+    </div>
+    
+  </div>
+  
+  <hr />
+  
+  <div class="feature-section col two-col">
+    
+    <div class="col-1">
+      
+      <p>Some video or image goes here</p>
+      
+    </div>
+    
+    <div class="col-2 last-feature">
+      
+      <h3><?php _e( 'Some another feature' ); ?></h3>
+      
+      <p>Another feature description</p>
+      
+    </div>
+    
+  </div>
+  
+</div>
