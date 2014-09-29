d520c0d0817aa811050b62b42d92945efaaff0b4
Anton Korotkov <antonkorotkoff@gmail.com>
2014-09-29 17:07:54 +0300

    install splash draft

--- static/splashes/install.php
+++ static/splashes/install.php
@@ -2,12 +2,17 @@
   
   <div class="overview">
 
-    <h2>WP-Invoice 4.0.0 has beed installed</h2>
+    <h2><?php _e( 'WP-Invoice 4.0.0 has beed installed', ud_get_wp_invoice()->domain ); ?></h2>
 
     <p><?php _e( 'Congratulations! You have just installed brand new version of WP-Invoice plugin. There are some important things that you need to be aware of in order to use our products successfully and with pleasure.', ud_get_wp_invoice()->domain ) ?></p>
 
     <p><?php _e( 'Please read the following instructions carefully. Explore links below to get more information on our site.', ud_get_wp_invoice()->domain ); ?></p>
 
+    <hr />
+    
+    <p><i><?php _e( 'WP-Invoice lets WordPress blog owners send itemized invoices to their clients. Ideal for web developers, SEO consultants, general contractors, or anyone with a WordPress blog and clients to bill.', ud_get_wp_invoice()->domain ); ?></i></p>
+    
+    <p><i><?php _e( 'In addition to the default invoicing function WP-Invoice can be extended with a <a href="">set of add-ons</a>. They may help you manage your business more effectively.', ud_get_wp_invoice()->domain ); ?></i></p>
   </div>
   
   <hr />
@@ -16,15 +21,30 @@
     
     <div class="col-1">
       
-      <h3><?php _e( 'New license management system', ud_get_wp_invoice()->domain ); ?></h3>
+      <h3><?php _e( 'User license management', ud_get_wp_invoice()->domain ); ?></h3>
       
-      <p>Information about licenses</p>
+      <h4><?php _e( 'Installation', ud_get_wp_invoice()->domain ); ?></h4>
+      
+      <p><?php _e( 'In new version of WP-Invoice all your premium features will be as separate plugins. If you are upgrading you will need to activate them one more time.', ud_get_wp_invoice()->domain ); ?></p>
+      
+      <p><?php _e( 'After you purchased the product, visit your UD Account. You will found license keys and download links for all your purchased add-ons. Download plugins to your computer and Upload as new plugin on your site.', ud_get_wp_invoice()->domain ); ?></p>
       
     </div>
     
     <div class="col-2 last-feature">
       
-      <p>Some video or image goes here</p>
+      <br /><br /><br />
+      
+      <h4><?php _e( 'Adding License Keys', ud_get_wp_invoice()->domain ); ?></h4>
+      
+      <p><?php _e( 'Click to activate plugin, you will see link to the Licenses admin screen. On the Licenses admin screen, you should see all your installed products, with an option to add your license key.', ud_get_wp_invoice()->domain ) ?></p>
+      
+      <p><?php _e( 'To add a license key:', ud_get_wp_invoice()->domain ); ?></p>
+      
+      <ul>
+        <li><?php _e( 'copy the license key from your UD Account page or receipt email;', ud_get_wp_invoice()->domain ); ?></li>
+        <li><?php _e( 'paste the license key into input field for your product;', ud_get_wp_invoice()->domain ); ?></li>
+      </ul>
       
     </div>
     
