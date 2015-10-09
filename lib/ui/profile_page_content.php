aa3cab634b1de34dd19c61bfd7169bd7f2313245
Anton Korotkov <antonkorotkoff@gmail.com>
2015-10-09 18:27:27 +0300

    removed unwanted print_r

--- lib/ui/profile_page_content.php
+++ lib/ui/profile_page_content.php
@@ -2,9 +2,9 @@
   global $wpi_settings;
   $user_information = apply_filters('wpi_user_information', $wpi_settings['user_meta']['custom']);
 
-echo '<pre>';
-print_r( $user_information );
-echo '</pre>';
+//echo '<pre>';
+//print_r( $user_information );
+//echo '</pre>';
 
   //$user_information = array_merge($wpi_settings['user_meta']['required'], $custom_user_information);
 ?>
