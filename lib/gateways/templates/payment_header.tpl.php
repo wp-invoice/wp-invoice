8d050cf5d524c63edf8783b83428f5d2b5f6fc47
Janeé Peña <diana@conekta.io>
2016-12-14 13:09:03 -0600

    Add ConektaJs to header

--- lib/gateways/templates/payment_header.tpl.php
+++ lib/gateways/templates/payment_header.tpl.php
@@ -1,5 +1,6 @@
 <?php if(!$from_ajax): ?>
 <!-- Pull in our JS -->
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<script type="text/javascript" src="https://conektaapi.s3.amazonaws.com/v0.3.2/js/conekta.js"></script>
 <div id="online_payment_form_wrapper" class="online_payment_form_wrapper">
 <?php endif; ?>
