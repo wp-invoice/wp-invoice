b2faf993b9018914b91c752dc6df4f774d4f52d7
Sudhir <sudhirpur123@gmail.com>
2017-02-16 21:49:44 +0530

    Fixes #70
    Fixed this as per the screenshot given.

--- lib/ui/profile_page_content.php
+++ lib/ui/profile_page_content.php
@@ -1,11 +1,18 @@
 <?php
   global $wpi_settings;
   $user_information = apply_filters('wpi_user_information', $wpi_settings['user_meta']['custom']);
+  $email = isset($wpi_settings['email_address']) ? "&email=".$wpi_settings['email_address']:'';
 ?>
-
-<h3><?php _e('Billing / Invoicing Info', ud_get_wp_invoice()->domain) ?></h3>
+<table class="form-table" ><tr>
+      <th><h3><?php _e('Billing / Invoicing Info', ud_get_wp_invoice()->domain) ?></h3></th>
+      <td>
+		<input type="button" onclick="window.location.href='<?php echo $wpi_settings['links']['manage_invoice'].$email; ?>'" class="button" value="<?php echo  __('Send New Invoice', ud_get_wp_crm()->domain) ?>" id="crm_new_invioce"/>
+      </td>
+    </tr>
+</table>
 <a name="billing_info"></a>
 <table class="form-table" >
+
   <?php foreach ($user_information as $field_id => $field_name) { ?>
     <tr>
       <th><?php _e($field_name, ud_get_wp_invoice()->domain) ?></th>
